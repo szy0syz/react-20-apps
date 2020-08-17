5bcdf823a31a8bf2a62763e29fb2f4f25c913f9b
szy0syz <szy0syz@gmail.com>
2020-08-17 22:53:14 +0800

    Finish A05

--- a05/src/App.js
+++ a05/src/App.js
@@ -1,10 +1,15 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
+import useMovement from './useMovement';
 import './App.css';
 
 export default function App() {
-  const canvasRef = useRef();
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const canvasRef = useRef(null);
+  const linkDownRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const linkLeftRef = useRef(null);
+
+  const { x, y, direction, move } = useMovement();
 
   // set the height and width of canvas
   useEffect(() => {
@@ -12,53 +17,54 @@ export default function App() {
     context.canvas.height = window.innerHeight;
     context.canvas.width = window.innerWidth;
 
-    context.fillRect(x, y, 100, 100);
-  }, []);
+    // context.fillRect(x, y, 100, 100);
+    context.drawImage(linkDownRef.current, x, y);
+  }, []); // eslint-disable-line
 
   // move the box if x or y changes
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.clearRect(0, 0, window.innerHeight, window.innerWidth);
-    context.fillRect(x, y, 100, 100);
-  }, [x, y]);
 
-  // add event listener to window to listen for arrow keys
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
+    // context.fillRect(x, y, 100, 100);
 
-    function handleKeyDown(e) {
-      if (e.key === 'ArrowUp') move('up');
-      if (e.key === 'ArrowLeft') move('left');
-      if (e.key === 'ArrowDown') move('down');
-      if (e.key === 'ArrowRight') move('right');
-    }
+    let theLinkRef;
+    if (direction === 'down') theLinkRef = linkDownRef;
+    if (direction === 'up') theLinkRef = linkUpRef;
+    if (direction === 'left') theLinkRef = linkLeftRef;
+    if (direction === 'right') theLinkRef = linkRightRef;
 
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, []);
-
-  function move(direction) {
-    if (direction === 'up') setY((y) => y - 20);
-    if (direction === 'left') setX((x) => x - 20);
-    if (direction === 'down') setY((y) => y + 20);
-    if (direction === 'right') setX((x) => x + 20);
-  }
+    context.drawImage(theLinkRef.current, x, y);
+  }, [x, y]); // eslint-disable-line
 
   return (
     <div className="app">
       <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button onClick={() => setY((x) => y - 20)}>Up</button>
-        <button onClick={() => setX((x) => x - 20)}>Left</button>
-        <button onClick={() => setY((x) => y + 20)}>Down</button>
-        <button onClick={() => setX((x) => x + 20)}>Right</button>
+        <button onClick={() => move('up')}>Up</button>
+        <button onClick={() => move('left')}>Left</button>
+        <button onClick={() => move('down')}>Down</button>
+        <button onClick={() => move('right')}>Right</button>
       </div>
 
       <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
       </div>
     </div>
   );
---
+++ a05/src/useMovement.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+export default () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [direction, setDirection] = useState('down');
+
+  // add event listener to window to listen for arrow keys
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      if (e.key === 'ArrowUp') move('up');
+      if (e.key === 'ArrowLeft') move('left');
+      if (e.key === 'ArrowDown') move('down');
+      if (e.key === 'ArrowRight') move('right');
+    }
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []); // eslint-disable-line
+
+  function move(dir) {
+    setDirection(dir);
+
+    if (dir === 'up') setY((y) => y - 20);
+    if (dir === 'left') setX((x) => x - 20);
+    if (dir === 'down') setY((y) => y + 20);
+    if (dir === 'right') setX((x) => x + 20);
+  }
+
+  return { x, y, direction, move };
+};
